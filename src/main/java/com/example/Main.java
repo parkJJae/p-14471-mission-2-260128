70eca0682bd26227f45bb606f66d57c552d8dbb2
parkJJae <wgpark0830@naver.com>
2026-02-03 15:13:29 +0900

    feat: 2번 요구사항 구현
    # Conflicts:
    #	src/main/java/com/example/Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,8 +7,23 @@ public class Main {
 
         System.out.println(" == 명언 앱 == ");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("명령)");
-        String command = scanner.nextLine();
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+            }
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+
 
     }
-}+
+}
