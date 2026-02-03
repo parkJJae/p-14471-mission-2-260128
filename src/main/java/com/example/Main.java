c67979600f0ee4339c06434708dac5abdac64bd6
parkJJae <wgpark0830@naver.com>
2026-02-03 15:00:53 +0900

    feat: 1번 요구사항 구현

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(" == 명언 앱 == ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("명령)");
+        scanner.nextLine();
+
+    }
+}+
