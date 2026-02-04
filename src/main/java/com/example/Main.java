dac4538f514b73a0ed725046e7a1aaeed03f57c4
parkJJae <wgpark0830@naver.com>
2026-02-04 10:21:29 +0900

    7번 요구사항 완료

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -42,14 +42,19 @@ public class Main {
             else if (command.startsWith("삭제?id=")){
                 String[] parts = command.split("="); // =을 만나면 짤라라. = 을 기준으로 짤라서 parts[0] -> 삭제?id , parts[1] -> 문자열
                 int targetId = Integer.parseInt(parts[1]); // 문자열이기 때문에 Int로 바꿔줘야함
+                boolean found = false;
                 for (int i = 0; i < wiseSayings.size(); i++) {
                     WiseSaying ws = wiseSayings.get(i);
                     if(ws.id == targetId){
                         wiseSayings.remove(i);
                         System.out.println(targetId + "번 명언이 삭제되었습니다.");
+                        found = true;
                         break;
                     }
 
+                }
+                if(!found){
+                    System.out.println(targetId + "번 명언은 존재하지 않습니다.");
                 }
             }
         }
